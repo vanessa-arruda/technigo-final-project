55fd02a94e84abbc4d701339cc653334b484d8b4
Vanessa Arruda <adm.vanessasouza@gmail.com>
2023-11-28 14:42:50 +0100

    add icons to navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import '../styles/footer.css';
+import { FaYoutube, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export const Footer = () => {
   return (
     <footer className='footer-container'>
         <div className='social-media'>
-            <a>LinkedIn</a>
-            <a>Instagram</a>
-            <a>YouTube</a>
+            <h3>Follow us</h3>
+            <a href="https://youtube.com"><FaYoutube/></a>
+            <a href="https://instagram.com"><FaInstagram /></a>
+            <a href="https://linkedin.com"><FaLinkedin /></a>
         </div>
         <p>Copyright 2023</p>
         <p>Created by Behrouz and Vanessa</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,36 @@
 import '../styles/navbar.css';
 import { NavLink, Link } from "react-router-dom";
+import { FaHome, FaChalkboardTeacher  } from "react-icons/fa";
+import { MdStars, MdDashboard, MdFormatListBulletedAdd } from "react-icons/md";
+import { FaClipboardQuestion } from "react-icons/fa6";
 
 export const Navbar = () => {
   return (
     <nav className='navBar'>
-        <NavLink to='/' className='navlink'>Home</NavLink>
-        <NavLink to='/rewards' className='navlink'>Rewards</NavLink>
-        <NavLink to='/history' className='navlink'>User history</NavLink>
-        <NavLink to='/tutorial' className='navlink'>Tutorial</NavLink>
-        <NavLink to='/quiz' className='navlink'>Quiz</NavLink>
-        <NavLink to='/add' className='navlink'>Add new item</NavLink>
+        <NavLink to='/' className='navlink'>
+            <FaHome />
+            <p>Home</p>
+        </NavLink>
+        <NavLink to='/rewards' className='navlink'>
+            <MdStars />
+            <p>Rewards</p>
+        </NavLink>
+        <NavLink to='/history' className='navlink'>
+            <MdDashboard/>
+            <p>User history</p>
+        </NavLink>
+        <NavLink to='/tutorial' className='navlink'>
+            <FaChalkboardTeacher/>
+            <p>Tutorial</p>
+        </NavLink>
+        <NavLink to='/quiz' className='navlink'>
+            <FaClipboardQuestion />
+            <p>Quiz</p>
+        </NavLink>
+        <NavLink to='/add' className='navlink'>
+            <MdFormatListBulletedAdd />
+            <p>Add new item</p>
+        </NavLink>
     </nav>
   )
 }
