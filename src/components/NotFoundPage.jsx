d77cc34b2119930000adfa1c61f7850969c10e0d
Vanessa Arruda <adm.vanessasouza@gmail.com>
2023-12-01 15:11:48 +0100

    add filter and sorting to rewards page

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,6 +1,14 @@
+import trashSearch from '../assets/trash-search.jpeg';
 
 export const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <div>
+        <h2>
+            Oh no! We can't find the page you are looking for.
+        </h2>
+        <p>try one of our links?</p>
+        <img src={trashSearch} alt='page not found - man searching inside bin'/>esn't exist
+    </div>
+    
   )
 }
